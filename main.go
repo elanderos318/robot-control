a1901971d4124846e73ae14bc07dfa347d43db4a
Eduardo Landeros <elanderos318@gmail.com>
2025-02-28 19:31:49 -0600

    Switch from TCP to UDP connection for robot communication

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ func main() {
 	// after connecting to the robot, found the ip address of the robot using the command:
 	// echo "Current IP: $(ipconfig getifaddr en0)"
 	const robotAddress = "192.168.4.2:80"
-	conn, err := net.Dial("tcp", robotAddress)
+	conn, err := net.Dial("udp", robotAddress)
 	if err != nil {
 		fmt.Println("Error connecting:", err)
 		return
 	}
+	
+	fmt.Println("Successfully connected to the robot!")
+
 	defer conn.Close()
 }
