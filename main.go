273bec95ea5d857be107732df657f250dac22533
Eduardo Landeros <elanderos318@gmail.com>
2025-02-28 18:40:56 -0600

    Update TCP connection with hardcoded robot IP address

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// after connecting to the robot, found the ip address of the robot using the command:
+	// echo "Current IP: $(ipconfig getifaddr en0)"
 	conn, err := net.Dial("tcp", "192.168.4.2:8080")
 	if err != nil {
 		fmt.Println("Error connecting:", err)
