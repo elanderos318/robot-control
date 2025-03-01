705c7d50cc29b45dc9eccda00ca87681eaf22e87
Eduardo Landeros <elanderos318@gmail.com>
2025-02-28 18:47:07 -0600

    Update robot connection port from 8080 to 80

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// after connecting to the robot, found the ip address of the robot using the command:
 	// echo "Current IP: $(ipconfig getifaddr en0)"
-	const robotAddress = "192.168.4.2:8080"
+	const robotAddress = "192.168.4.2:80"
 	conn, err := net.Dial("tcp", robotAddress)
 	if err != nil {
 		fmt.Println("Error connecting:", err)
